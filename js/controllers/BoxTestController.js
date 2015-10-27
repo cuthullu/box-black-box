3666d8ca84052da2ff61c92decad51ee6f6caad3
Thomas Hull <thull@scottlogic.co.uk>
2015-10-27 14:03:24 +0000

    Added validation

--- js/controllers/BoxTestController.js
+++ js/controllers/BoxTestController.js
@@ -6,6 +6,10 @@
         this.box2 = box2;
         this.results = [];
         this.expected = true;
+
+        this.isValid = function() {
+            return box1.isValid() && box2.isValid();
+        }
     }
 
     function Box(x, y, width, height) {
@@ -13,6 +17,18 @@
         this.y = y;
         this.width = width;
         this.height = height;
+        var self = this;
+        
+        this.isValid = function() {
+            return isInt(self.x) && 
+                isInt(self.y) &&
+                isInt(self.width) &&
+                isInt(self.height);
+        }
+
+        function isInt(i) {
+            return i === parseInt(i, 10);
+        }
     }
 
     function BoxTestController(algorithmService) {
@@ -29,14 +45,21 @@
         }
 
         vm.plusTest = newTest;
+
         function runTestSuit(testCase) {
             testCase.results = [];
-            vm.algorithms.forEach(function(algorithm) {
-                testCase.results.push({
-                    "algorithm" : algorithm.name,
-                    "outCome" : algorithm.run(testCase.box1, testCase.box2)
-                });
-            })
+            testCase.errors = "";
+
+            if(testCase.isValid()){
+                vm.algorithms.forEach(function(algorithm) {
+                    testCase.results.push({
+                        "algorithm" : algorithm.name,
+                        "outCome" : algorithm.run(testCase.box1, testCase.box2)
+                    });
+                })
+            } else {
+                testCase.errors = "Test case contains invalid values";
+            }
         }
 
         function newTest() {
@@ -47,53 +70,60 @@
         }
         newTest();
 
-
-        //
         // var box1 = new Box(0,0,4,4)
         // var box2 = new Box(1,1,4,4)
         // vm.testCases.push(new TestCase(box1,box2))
-        //
+
         // box2 = new Box(0,0,4,4)
         // box1 = new Box(1,1,4,4)
         // vm.testCases.push(new TestCase(box1,box2))
-        //
+
         // box1 = new Box(2,0,2,4)
         // box2 = new Box(1,1,4,2)
         // vm.testCases.push(new TestCase(box1,box2))
-        //
+
         // box2 = new Box(2,0,2,4)
         // box1 = new Box(1,1,4,2)
         // vm.testCases.push(new TestCase(box1,box2))
-        //
+
         // box1 = new Box(0,0,2,2)
         // box2 = new Box(1,3,2,2)
         // var testCase = new TestCase(box1,box2);
         // testCase.expected = false;
         // vm.testCases.push(testCase);
-        //
-        // box1 = new Box(0,0,2,2)
-        // box2 = new Box(3,1,2,2)
+
+        // box2 = new Box(0,0,2,2)
+        // box1 = new Box(3,1,2,2)
         // var testCase = new TestCase(box1,box2);
         // testCase.expected = false;
         // vm.testCases.push(testCase);
-        //
+
         // box1 = new Box(0,0,2,2)
         // box2 = new Box(2,2,2,2)
         // var testCase = new TestCase(box1,box2);
         // testCase.expected = false;
         // vm.testCases.push(testCase);
-        //
+
         // box1 = new Box(2,2,2,2)
         // box2 = new Box(2,2,2,2)
         // var testCase = new TestCase(box1,box2);
         // vm.testCases.push(testCase);
-        //
+
         // box1 = new Box(2,2,2,2)
         // box2 = new Box(1,1,3,3)
         // var testCase = new TestCase(box1,box2);
         // vm.testCases.push(testCase);
 
+        // box1 = new Box(0,0,2,2)
+        // box2 = new Box(0,2,2,2)
+        // var testCase = new TestCase(box1,box2);
+        // vm.testCases.push(testCase);
 
+        // box1 = new Box(2,0,3,1)
+        // box2 = new Box(0,0,1,2)
+        // var testCase = new TestCase(box1,box2);
+        // testCase.expected = false;
+        // vm.testCases.push(testCase);
     }
 
 })();
