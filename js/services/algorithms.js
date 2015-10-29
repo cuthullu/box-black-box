1ae518aacd0c4d29bb73e2195f299a2b59f29c39
Thomas Hull <thull@scottlogic.co.uk>
2015-10-29 12:03:32 +0000

    Reversed algorithm b fixes #12

--- js/services/algorithms.js
+++ js/services/algorithms.js
@@ -48,7 +48,7 @@
         }
 
         //correct answer without reversing
-        function b(box1, box2) {
+        function b(box2, box1) {
             if(((box1.y >= box2.y && box1.x >= box2.x) &&
                 (box1.y < (box2.y + box2.height) && box1.x < (box2.x + box2.width) )) ||
                 ((box1.y >= box2.y && box1.x + box1.width <= box2.x + box1.width) &&
